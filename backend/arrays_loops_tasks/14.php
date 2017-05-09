826aa08f77e69093b8819f4c442ed5111a45eecc
Andriy <Andriy.roshchin@gmail.com>
2017-05-09 21:03:09 +0300

    some fixes

--- backend/arrays_loops_tasks/14.php
+++ backend/arrays_loops_tasks/14.php
@@ -1,14 +1,18 @@
 <?php
 $arr = [4, 2, 5, 19, 13, 0, 10];
-$e = 2;
+$e = [2, 3, 4];
+$haveElementsE = false;
 
 foreach ($arr as $number) {
-	if ($number == $e) {
-		echo 'Есть!'.PHP_EOL;
-	} else {
-		echo 'Нет!'.PHP_EOL;
-
-	}
+    if (in_array($number, $e)) {
+        $haveElementsE = true;
+        break;
+    }
 }
 
+if ($haveElementsE) {
+        echo 'Есть!'.PHP_EOL;
+    } else {
+        echo 'Нет!'.PHP_EOL;
+    }
 ?>
